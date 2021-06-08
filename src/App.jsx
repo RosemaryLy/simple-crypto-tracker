99c7a3a8f1b02acd19ccb2d622f63689f8cdc58f
RosemaryLy <rosey.ly@hotmail.com>
2021-06-08 00:41:53 -0400

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { WatchListContextProvider } from "./context/watchListContext";
 
 const App = () => {
   return (
-    <div className="container">
+    <div className="container mx-auto">
       <WatchListContextProvider>
         <BrowserRouter>
           <Header />
